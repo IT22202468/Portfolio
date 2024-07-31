19f382232e00a439f6bc36ab162925c32b21d95e
Nipun Pramodya Jayasinghe <nipunprjay@gmail.com>
2024-07-31 17:12:13 +0530

    Added styles to navbar

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -1,23 +1,42 @@
 import React from "react";
 import Link from "next/link";
+import NavLink from "./NavLink";
+
+const navLinks = [
+    {
+        title: "About",
+        path: "#about"
+    },
+    {
+        title: "Projects",
+        path: "#projects"
+    },
+    {
+        title: "Blog",
+        path: "#blog"
+    },
+    {
+        title: "Contact",
+        path: "#contact"
+    }
+]
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className="flex flex-wrap items-center justify-between p-8 mx-auto">
-        <Link href={"/"} className="text-5xl font-semibold text-white">
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
+      <div className="flex flex-wrap items-center justify-between px-4 mx-auto">
+        <Link href={"/"} className="text-2xl font-semibold text-white md:text-5xl">
           LOGO
         </Link>
-        <div className="hidden menu md:block md:w-auto" id="navbar">
-          <ul>
-            <li>
-              <Link
-                href={"#about"}
-                className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
-              >
-                About
-              </Link>
-            </li>
+        <div className="block menu md:w-auto" id="navbar">
+          <ul className="flex p-4 mt-0 md:flex-row md:p-0 md:space-x-8">
+            {
+                navLinks.map((link, index) => (
+                    <li key={index}>
+                        <NavLink href={link.path} title={link.title}/>
+                    </li>
+                ))
+            }
           </ul>
         </div>
       </div>
--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar/>
-      <div className="container px-12 py-4 mx-auto">
+      <div className="container px-12 py-4 mx-auto mt-24">
       <HeroSection/>
       </div>
     </main>
