9bbcb8deb8afa329e3982ed279cfab889c34253e
Nipun Pramodya Jayasinghe <nipunprjay@gmail.com>
2024-08-01 01:13:33 +0530

    Started implementing "Projects" section

---
+++ src/app/Components/ProjectsSection.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import ProjectCard from './ProjectCard'
+
+const projectsData = [
+    {
+        id: 1,
+        title: "The Official Website of Mini Hackathon - 2024 ",
+        description: "Open-Source Project by MS Club of SLIIT (Ongoing project). Contributed to implement the Rules Section.",
+        image: "",
+        tag: ["All", "Web"]
+    },
+    {
+        id: 2,
+        title: "The Official Website of Computer Science and Networking Engineering Student Community of SLIIT 2024",
+        description: "Open-Source Project by MS Club of SLIIT (Ongoing project). Contributed to fix issues in frontend.",
+        image: "",
+        tag: ["All", "Web"]
+    },
+    {
+        id: 3,
+        title: "Web Application for Genius Restaurant (Restaurant Management System) – 2024",
+        description: "Developed a web application using MERN stack for Genius Restaurant to manage their restaurant tasks easily.",
+        image: "",
+        tag: ["All", "Web"]
+    },
+    {
+        id: 4,
+        title: "Event Management System -2023",
+        description: "",
+        image: "Developed a web application using Java as backend.",
+        tag: ["All", "Web"]
+    },
+    {
+        id: 5,
+        title: "Tuk Tuk",
+        description: "Developed a simple android game using Kotlin",
+        image: "",
+        tag: ["All", "Mobile"]
+    }
+]
+
+const ProjectsSection = () => {
+  return (
+    <><h2>My Projects</h2>
+    <div>
+
+    </div>
+    </>
+  )
+}
+
+export default ProjectsSection
