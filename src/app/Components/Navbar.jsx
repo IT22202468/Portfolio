637b2c47ba1e4b25fa1397f32c16c1547bba0a8f
Nipun Pramodya Jayasinghe <nipunprjay@gmail.com>
2024-07-31 18:05:09 +0530

    MenuOverlay created. but mobile navbar is not working yet

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
 import NavLink from "./NavLink";
+import { Bars3icon, XCircleIcon } from "@heroicons/react/24/solid";
+import MenuOverlay from "./MenuOverlay";
 
 const navLinks = [
     {
@@ -22,26 +25,38 @@ const navLinks = [
 ]
 
 const Navbar = () => {
-  return (
-    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
-      <div className="flex flex-wrap items-center justify-between px-4 mx-auto">
-        <Link href={"/"} className="text-2xl font-semibold text-white md:text-5xl">
-          LOGO
-        </Link>
-        <div className="block menu md:w-auto" id="navbar">
-          <ul className="flex p-4 mt-0 md:flex-row md:p-0 md:space-x-8">
-            {
-                navLinks.map((link, index) => (
-                    <li key={index}>
-                        <NavLink href={link.path} title={link.title}/>
-                    </li>
-                ))
-            }
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+    const [navbarOpen, setNavbarOpen] = useState(false);
+
+    return (
+        <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
+            <div className="flex flex-wrap items-center justify-between px-4 py-2 mx-auto">
+                <Link href={"/"} className="text-2xl font-semibold text-white md:text-5xl">
+                    LOGO
+                </Link>
+                <div className="block mobile-menu md:hidden">
+                    {
+                        navbarOpen ? (
+                            <button onClick={() => setNavbarOpen(true)} className="flex items-center px-3 py-2 border text-slate-200 border-slate-200 hover:text-white hover:border-white"><Bars3icon className="w-5 h-5"/></button>
+                        ) : (
+                            <button onClick={() => setNavbarOpen(false)} className="flex items-center px-3 py-2 border text-slate-200 border-slate-200 hover:text-white hover:border-white"><XCircleIcon className="w-5 h-5"/></button>
+                        )
+                    }
+                </div>
+                <div className="hidden md:block menu md:w-auto" id="navbar">
+                    <ul className="flex p-4 mt-0 md:flex-row md:p-0 md:space-x-8">
+                        {
+                            navLinks.map((link, index) => (
+                                <li key={index}>
+                                    <NavLink href={link.path} title={link.title} />
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+            </div>
+            {navbarOpen ? <MenuOverlay links={navLinks}/>: null}
+        </nav>
+    );
 };
 
 export default Navbar;
