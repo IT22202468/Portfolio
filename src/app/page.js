7c32f8e3fd95d1483070c86e42f966a288188976
Nipun Pramodya Jayasinghe <nipunprjay@gmail.com>
2024-07-31 14:57:04 +0530

    Navbar created

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import HeroSection from "./Components/HeroSection";
-
+import Navbar from "./Components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
+      <Navbar/>
       <div className="container px-12 py-4 mx-auto">
       <HeroSection/>
       </div>
