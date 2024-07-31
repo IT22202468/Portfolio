158781fbcbf5b475d0d53545adb7037222bbdb36
Nipun Pramodya Jayasinghe <nipunprjay@gmail.com>
2024-07-31 14:44:30 +0530

    Hero Section done

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
+import HeroSection from "./Components/HeroSection";
 
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <div className="container px-12 py-4 mx-auto">
+      <HeroSection/>
+      </div>
     </main>
   );
 }
